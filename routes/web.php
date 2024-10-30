a5c238c88e9e4e2588cb6bc0103147af5c1fa21a
NofaFirdaus <nofafirdaus17@gmail.com>
2024-10-31 01:06:42 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +20,18 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware('admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/ProfileStore', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/change/password', [AdminController::class, 'changePassword'])->name('profile.changePassword');
+    Route::post('/change/password', [AdminController::class, 'ForgetPassword'])->name('admin.password.update');
+});
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+Route::post('/admin/login', [AdminController::class, 'AdminLoginSubmit'])->name('admin.submitLogin');
+Route::get('/admin/forgetPassword',[AdminController::class, 'ForgetPassword'])->name('admin.forgetPassword');
+
+Route::post('/admin/forgetPassword', [AdminController::class, 'passwordGenerate'])->name('admin.password_generate');
