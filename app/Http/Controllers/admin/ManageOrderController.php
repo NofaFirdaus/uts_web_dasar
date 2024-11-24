3aa0ff30453ecc68a0fefe97b31fa5d39e6b1b0f
NofaFirdaus <nofafirdaus17@gmail.com>
2024-11-25 00:26:46 +0700

    folder d no 34

--- app/Http/Controllers/admin/ManageOrderController.php
+++ app/Http/Controllers/admin/ManageOrderController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Order;
 use App\Models\OrderItem;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
@@ -82,29 +83,48 @@ class ManageOrderController extends Controller
             ->groupBy('order_id');
         return view('client.backend.order.all_orders', compact('orderItemGroupData'));
     }
-    public function ClientOrderDetails($id){
-        $order = Order::with('user')->where('id',$id)->first();
-        $orderItem = OrderItem::with('product')->where('order_id',$id)->orderBy('id','desc')->get();
+    public function ClientOrderDetails($id)
+    {
+        $cid = Auth::guard('client')->id();
+        $order = Order::with('user')->where('id', $id)->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $id)->where('client_id', $cid)->orderBy('id', 'desc')->get();
         $totalPrice = 0;
-        foreach($orderItem as $item){
+        foreach ($orderItem as $item) {
             $totalPrice += $item->price * $item->qty;
         }
-        return view('client.backend.order.client_order_details',compact('order','orderItem','totalPrice'));
+        return view('client.backend.order.client_order_details', compact('order', 'orderItem', 'totalPrice'));
     }
 
-    public function UserOrderList(){
+    public function UserOrderList()
+    {
         $userId = Auth::user()->id;
-        $allUserOrder = Order::where('user_id',$userId)->orderBy('id','desc')->get();
-        return view('frontend.dashboard.order.order_list',compact('allUserOrder'));
+        $allUserOrder = Order::where('user_id', $userId)->orderBy('id', 'desc')->get();
+        return view('frontend.dashboard.order.order_list', compact('allUserOrder'));
     }
 
-    public function UserOrderDetails($id){
-        $order = Order::with('user')->where('id',$id)->where('user_id',Auth::id())->first();
-        $orderItem = OrderItem::with('product')->where('order_id',$id)->orderBy('id','desc')->get();
+    public function UserOrderDetails($id)
+    {
+        $order = Order::with('user')->where('id', $id)->where('user_id', Auth::id())->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $id)->orderBy('id', 'desc')->get();
         $totalPrice = 0;
-        foreach($orderItem as $item){
+        foreach ($orderItem as $item) {
             $totalPrice += $item->price * $item->qty;
         }
-        return view('frontend.dashboard.order.order_details',compact('order','orderItem','totalPrice'));
+        return view('frontend.dashboard.order.order_details', compact('order', 'orderItem', 'totalPrice'));
+    }
+
+    public function UserInvoiceDownload($id)
+    {
+        $order = Order::with('user')->where('id', $id)->where('user_id', Auth::id())->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $id)->orderBy('id', 'desc')->get();
+        $totalPrice = 0;
+        foreach ($orderItem as $item) {
+            $totalPrice += $item->price * $item->qty;
+        }
+        $pdf = Pdf::loadView('frontend.dashboard.order.invoice_download', compact('order', 'orderItem', 'totalPrice'))->setPaper('a4')->setOption([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+        ]);
+        return $pdf->download('invoice.pdf');
     }
 }
